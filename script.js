2fb01f2aa01e2c180beb5a6fc29c0686cc89a412
zmaderow <88632400+zmaderow@users.noreply.github.com>
2023-08-24 23:06:49 -0300

    💥 fix: atualizando variáveis de armazenamento e erros de design

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
+backgroundColor = localStorage.getItem("backgroundColor") ? localStorage.getItem("backgroundColor") : localStorage.setItem("backgroundColor", "#000")
+detailColor = localStorage.getItem("detailColor") ? localStorage.getItem("detailColor") : localStorage.setItem("detailColor", "#000")
+fontColor = localStorage.getItem("fontColor") ? localStorage.getItem("fontColor") : localStorage.setItem("fontColor", "#fff")
+
+async function onLoad() {
+    document.documentElement.style.setProperty('--body-bg', backgroundColor);
+    document.documentElement.style.setProperty('--detail-color', detailColor);
+    document.documentElement.style.setProperty('--font-color', fontColor);
+}
+
 function turnMode(detail, background, font) {
+    localStorage.setItem("backgroundColor", background)
+    localStorage.setItem("detailColor", detail)
+    localStorage.setItem("fontColor", font)
     document.documentElement.style.setProperty('--body-bg', background);
     document.documentElement.style.setProperty('--detail-color', detail);
     document.documentElement.style.setProperty('--font-color', font);
